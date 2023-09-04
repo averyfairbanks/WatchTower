857c620096fc1e3616a4cdfcade8715be0927a22
Avery <avery@averyf.dev>
2023-09-04 14:52:23 -0400

    handle decoding errors in controller and empty meals array in client

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,3 +1,12 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
 export const decode = (encodedId: string) => {
+  try {
     return parseInt(atob(encodedId));
-}
+  } catch (err) {
+    if (err instanceof DOMException) {
+      throw new HttpException("ID couldn't be decoded.", HttpStatus.BAD_REQUEST);
+    }
+    throw new HttpException("UNKNOWN exception in decode method.", HttpStatus.AMBIGUOUS);
+  }
+};
