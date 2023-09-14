55a8dcb7021a08bea937dfb6a3a803c681228d43
Avery <avery@averyf.dev>
2023-09-14 16:54:36 -0400

    small updates to meal card for readability

--- client/src/Meal/types.tsx
+++ client/src/Meal/types.tsx
@@ -2,7 +2,7 @@ export interface UserMeal {
   id: number;
   userId: number;
   name: string;
-  photoUrl: URL;
+  photoUrl: string;
   description: string;
   timeLogged: string;
   notified: boolean;
